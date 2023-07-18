8fb6233ce25c25a6d6f2020f04113211726ed649
DiegoGomezCuadross <digoc_123hotmail.comm>
2023-07-17 19:29:10 -0500

    add: validates property

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,11 @@
 class Property < ApplicationRecord
   belongs_to :user
+
+    validates :operation_type, presence: true
+    validates :address, presence: true
+    validates :property_type, presence: true
+    validates :bathrooms, presence: true
+    validates :bedrooms, presence: true
+    validates :area, presence: true
+    validates :about, presence: true
 end
